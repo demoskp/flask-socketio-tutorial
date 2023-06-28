5025008e71f0ba141f06529ce1375e510c08add9
Demos Petsas <demos@petsas.consulting>
2023-06-28 18:21:58 +0300

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import emit, join_room, leave_room, rooms
+from flask_socketio import rooms, emit, join_room, leave_room
 
 from extensions import socketio
 
@@ -8,36 +8,35 @@ app.config.from_object("config")
 socketio.init_app(app)
 
 
-@socketio.on('connect')
-def connect(auth):
-    print(auth)
+@socketio.on("connect")
+def on_connect(auth):
     if auth.get("token") != "123":
-        raise ConnectionRefusedError('unauthorized!')
+        raise ConnectionRefusedError("unauthorized!")
 
 
-@socketio.on('message')
+@socketio.on("message")
 def handle_message(data):
-    username = data['username']
-    message = data['message']
-    room = data['room']
+    username = data.get("username")
+    message = data.get("message")
+    room = data.get("room")
     if room in rooms():
-        emit('message', {'username': username, 'message': message}, to=room)
+        emit("message", {"username": username, "message": message}, to=room)
 
 
-@socketio.on('join')
+@socketio.on("join")
 def on_join(data):
-    username = data['username']
-    room = data['room']
+    username = data.get("username")
+    room = data.get("room")
     join_room(room)
-    emit('join', {'username': username}, to=room)
+    emit("join", {"username": username}, to=room)
 
 
-@socketio.on('leave')
+@socketio.on("leave")
 def on_leave(data):
-    username = data['username']
-    room = data['room']
+    username = data.get("username")
+    room = data.get("room")
     leave_room(room)
-    emit('leave', {'username': username}, to=room)
+    emit("leave", {"username": username}, to=room)
 
 
 if __name__ == "__main__":
